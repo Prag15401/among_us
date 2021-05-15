08b34e1b184245fb917af0780e1798e745ee8720
Prag15401 <83070122+Prag15401@users.noreply.github.com>
2021-05-15 11:37:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const red = document.getElementById("red")
+const blue = document.getElementById("blue")
+const yellow = document.getElementById("yellow")
+
+const red = document.getElementById("red")
+const blue = document.getElementById("blue")
+const yellow = document.getElementById("yellow")
+
+const button = document
